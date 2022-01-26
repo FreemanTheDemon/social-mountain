c5078de077c6f132a84bf1d3fca213a994aab632
Freeman Bird <freemanbird@gmail.com>
2022-01-26 13:42:42 -0700

    completed the assignment, some minor promise errors might exist

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,14 +7,26 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: ''
+    };
+  }
+
+  handleInput(e) {
+    this.setState({input: e.target.value});
+  }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={this.handleInput.bind(this)} />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={() => {this.props.searchPostFn(this.state.input)}}/>
         </div>
         
       </section>
--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -20,7 +20,9 @@ export default class Edit extends Component {
   }
 
   updatePost() {
-
+    const { id, updatePostFn, hideEdit } = this.props
+    updatePostFn(id, this.state.text);
+    hideEdit();
   }
 
   render() {
